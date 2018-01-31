37b583f035a122701c06894f50239f5ccbc5b49e
Noah <noahcoetsee@gmail.com>
2018-01-31 15:14:32 -0500

    Update ChannelUtils.java

--- src/main/java/fr/triinoxys/callme/utils/ChannelUtils.java
+++ src/main/java/fr/triinoxys/callme/utils/ChannelUtils.java
@@ -1,20 +1,20 @@
-package fr.triinoxys.callme.utils;
+package net.fathomtech.plugins.CityPlus.Utilities;
 
 import org.bukkit.entity.Player;
-import fr.triinoxys.callme.Main;
-import fr.triinoxys.callme.handlers.Channel;
+import net.fathomtech.plugins.CityPlus.Main;
+import net.fathomtech.plugins.CityPlus.Handlers.Channel;
 
 
-public class ChannelUtils{
+public class ChannelUtils {
     
-    public static Channel getChannel(Player p){
+    public static Channel getChannel(Player p) {
         Channel channel = null;
         for(Channel c : Main.channels) 
             if(c.hasMember(p)) channel = c;
         return channel;
     }
     
-    public static boolean isCaller(Player p){
+    public static boolean isCaller(Player p) {
         if(getChannel(p) != null && getChannel(p).getCaller() == p) return true;
         else return false;
     }
