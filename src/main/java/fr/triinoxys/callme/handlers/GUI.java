e7e888cd97e22cfa5f417f00be0752f847e1775b
Noah <noahcoetsee@gmail.com>
2018-01-31 15:12:26 -0500

    Update GUI.java

--- src/main/java/fr/triinoxys/callme/handlers/GUI.java
+++ src/main/java/fr/triinoxys/callme/handlers/GUI.java
@@ -1,4 +1,4 @@
-package fr.triinoxys.callme.handlers;
+package net.fathomtech.plugins.CityPlus.Handlers;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -12,59 +12,59 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class GUI{
     
     public static void openMenu(Player p){
-        Inventory menu = Bukkit.createInventory(null, 9, "§6§lTéléphone");
+        Inventory menu = Bukkit.createInventory(null, 9, "Â§6Â§liHaven");
          
         ItemStack appel = new ItemStack(Material.RECORD_11);
         ItemMeta appelMeta = appel.getItemMeta();
-        appelMeta.setDisplayName(ChatColor.GOLD + "Appel");
+        appelMeta.setDisplayName(ChatColor.GOLD + "Call");
         appel.setItemMeta(appelMeta);
         menu.setItem(0, appel);
         
         ItemStack ajout = new ItemStack(Material.RECORD_12);
         ItemMeta ajoutMeta = ajout.getItemMeta();
-        ajoutMeta.setDisplayName(ChatColor.GOLD + "Ajout");
+        ajoutMeta.setDisplayName(ChatColor.GOLD + "Add");
         ajout.setItemMeta(ajoutMeta);
         menu.setItem(1, ajout);
         
         ItemStack oui = new ItemStack(Material.RECORD_3);
         ItemMeta ouiMeta = oui.getItemMeta();
-        ouiMeta.setDisplayName(ChatColor.GOLD + "Accepter");
+        ouiMeta.setDisplayName(ChatColor.GOLD + "Answer");
         oui.setItemMeta(ouiMeta);
         menu.setItem(2, oui);
         
         ItemStack non = new ItemStack(Material.RECORD_4);
         ItemMeta nonMeta = non.getItemMeta();
-        nonMeta.setDisplayName(ChatColor.GOLD + "Refuser");
+        nonMeta.setDisplayName(ChatColor.GOLD + "Deny");
         non.setItemMeta(nonMeta);
         menu.setItem(3, non);
         
         ItemStack raccrocher = new ItemStack(Material.RECORD_5);
         ItemMeta raccrocherMeta = raccrocher.getItemMeta();
-        raccrocherMeta.setDisplayName(ChatColor.GOLD + "Raccrocher");
+        raccrocherMeta.setDisplayName(ChatColor.GOLD + "Hangup");
         raccrocher.setItemMeta(raccrocherMeta);
         menu.setItem(4, raccrocher);
         
         ItemStack fin = new ItemStack(Material.RECORD_6);
         ItemMeta finMeta = fin.getItemMeta();
-        finMeta.setDisplayName(ChatColor.GOLD + "Mettre fin à l'appel");
+        finMeta.setDisplayName(ChatColor.GOLD + "Stop the Call");
         fin.setItemMeta(finMeta);
         menu.setItem(5, fin);
         
         ItemStack global = new ItemStack(Material.RECORD_7);
         ItemMeta globalMeta = global.getItemMeta();
-        globalMeta.setDisplayName(ChatColor.GOLD + "Parler en mode Global");
+        globalMeta.setDisplayName(ChatColor.GOLD + "Talk in Global");
         global.setItemMeta(globalMeta);
         menu.setItem(6, global);
         
         ItemStack sms = new ItemStack(Material.RECORD_8);
         ItemMeta smsMeta = sms.getItemMeta();
-        smsMeta.setDisplayName(ChatColor.GOLD + "Envoyer un SMS");
+        smsMeta.setDisplayName(ChatColor.GOLD + "Send a Message");
         sms.setItemMeta(smsMeta);
         menu.setItem(7, sms);
         
         ItemStack smslist = new ItemStack(Material.RECORD_9);
         ItemMeta smslistMeta = smslist.getItemMeta();
-        smslistMeta.setDisplayName(ChatColor.GOLD + "Liste des SMS");
+        smslistMeta.setDisplayName(ChatColor.GOLD + "Message History");
         smslist.setItemMeta(smslistMeta);
         menu.setItem(8, smslist);
         
